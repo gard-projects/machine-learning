cd001309a02752a8a97c58a13c64f15d2bbc9ac8
gardgundersen44 <gardgundersen4@gmail.com>
2024-02-11 17:23:45 +0100

    v 1.1.1

--- LStart.py
+++ LStart.py
@@ -2,41 +2,52 @@ import pandas as pd
 import numpy as np
 
 class LinReg():
-    def __init__(self, epoch=10, learning_rate=0.01):
+    def __init__(self, epoch=10, learning_rate=0.001, stop_criterion=1e-6):
+        self.theta= None # Weights matrix
         self.epoch = epoch
         self.learning_rate = learning_rate
+        self.stop_criterion = stop_criterion
         
     def fit(self, training_set):
-        # Intialize weight matrix 
-        # Weight matrix: (m+1, 1)
-        # Feature matrix: (n, m+1)
-        m = training_set.shape[1] - 1 # number of features (indepedent variables)
-        n = training_set.shape[0] # number of samples (observations)
-        
-        weight_mtx = np.random.rand(m+1, 1)
-        feature_mtx = np.hstack((np.ones((n,1)), training_set[:, :m])) # Add bias term
-        actual_response = training_set[:, m:]
-        
-        # Calculate predicted response
-        predicted_response = np.dot(feature_mtx, weight_mtx)
-        self.loss_function(actual_response, predicted_response, m)
-    
-    def loss_function(self, actual_response, predicted_response, m: int) -> float:
-        j = (1/(2*m)) * np.sum((predicted_response - actual_response)**2) # Mean Squared Error
-        return j
-    
-    def gradient_descent(self):
-        # Something here...
-        return
+      m = training_set.shape[1]-1   # number of predictors
+      n = training_set.shape[0]     # number of samples
+      previous_cost = 0
+      
+      # Split dataset into predictors and response
+      X = training_set[0:n, 0:m]
+      y = training_set[0:n, -1:]
+      
+      # Design matrix (include bias term)
+      X = np.c_[(np.ones(n), X)]
+
+      # Random initialization of weights
+      self.theta = np.random.rand(m+1, 1)
+         
+      # Gradient descent
+      for i in range(self.epoch):
+          grad = self.gradient_descent(n, X, y)
+          self.theta = self.theta - self.learning_rate * grad
+          # Calculate the distance score (loss) for each epoch
+          error = self.cost_function(n, X, y)
+          print("Error estimate:", error, "Epoch:", i+1)
+      
+      return 0
+  
+    def cost_function(self, n, X, y):
+        '''J(theta), uses mean squared error (MSE)'''
+        J = 1/(2*n) * np.sum(np.square(np.matmul(X,self.theta) - y))
+        return J
+       
+    def gradient_descent(self, n, X, y):
+        grad = 1/n * np.matmul(X.T, np.matmul(X, self.theta) - y)
+        return grad
     
 if __name__ == "__main__":
     # Load dataset
     training_data = pd.read_csv("dataset/l1_train.csv").to_numpy()
     testing_data = pd.read_csv("dataset/l1_test.csv").to_numpy()
     
-    # Linear regression
-    linreg = LinReg(epoch=5, learning_rate=0.01)
-    linreg.fit(training_data)
-    
+    model = LinReg(epoch=100)
+    model.fit(training_data)
     
     
