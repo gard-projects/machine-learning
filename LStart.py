fc79d43d490f7d09fd6812065a1597bf5c609d79
gardgundersen44 <gardgundersen4@gmail.com>
2024-02-24 21:30:30 +0100

    v 1.1.6

--- LStart.py
+++ LStart.py
@@ -6,77 +6,73 @@ from sklearn import linear_model
 # Allowing virtual environment to be used: Set-ExecutionPolicy Unrestricted -Scope Process
 
 class LinReg():
-    def __init__(self, epoch=10, learning_rate=0.001, stop_criterion=1e-6):
-        self.theta= None # Weights matrix
-        self.epoch = epoch
+    def __init__(self, epochs, learning_rate):
+        self.epochs = epochs
         self.learning_rate = learning_rate
-        self.stop_criterion = stop_criterion
+        self.theta= np.zeros(0)
+        self.cost_history = [] # Keep track of the distance score
         
-    def fit(self, training_set):
-      m = training_set.shape[1]-1   # number of predictors
-      n = training_set.shape[0]     # number of samples
-      
-      # Split dataset into predictors and response
-      X_features = training_set[0:n, 0:m]
-      y = training_set[0:n, -1:]
-      
-      # Design matrix (include bias term)
-      X = np.c_[(np.ones(n), X_features)]
-
-      # Random initialization of weights
-      self.theta = np.random.rand(m+1, 1)
-         
-      # Gradient descent
-      for i in range(self.epoch):
-          grad = self.gradient_descent(n, X, y)
-          self.theta = self.theta - (self.learning_rate * grad)
-      return 0
-  
-    def cost_function(self, n, X, y):
-        '''J(theta), uses mean squared error (MSE)'''
-        # Altenrative: J = 1/(2*n) * np.sum(np.square(np.matmul(X,self.theta) - y))
-        J = 1/(2*n) * np.matmul((np.matmul(X,self.theta) - y).T, (np.matmul(X,self.theta) - y))
+    def fit(self, X_features: np.ndarray, y: np.ndarray):
+        '''Find the weights for the linear regression model'''
+        n = X_features.shape[0] # Number of samples
+        m = X_features.shape[1] # Number of predictors (features)
+        
+        # Setup design matrix
+        X = np.c_[np.ones(n), X_features]
+        
+        # Random initialization of weights
+        self.theta = np.random.rand(m+1, 1)
+    
+        # Gradient Descent
+        for _ in range(self.epochs):
+            self.theta -= self.learning_rate * self.gradient(X, y, n)
+            print("Grad:", self.gradient(X, y, n))
+            self.cost_history.append(self.cost_function(X, y, n))
+        return 0
+    
+    def gradient (self, X: np.ndarray, y: np.ndarray, n: int) -> np.ndarray:
+        '''Calculates the gradient for the given matrices X and y'''
+        y_est = self.predict(X)
+        e = y_est - y # Error vector
+        g = (1/n) * np.dot(X.T, e)
+        return g
+    
+    def cost_function(self, X: np.ndarray, y: np.ndarray, n: int) -> float:
+        '''Calculates the distance score using the MSE cost function''' 
+        y_est = self.predict(X)
+        e = y_est - y # Error vector
+        J = (1/(2*n)) * np.dot(e.T, e)
         return J
-
-    def gradient_descent(self, n, X, y):
-        grad = (1/n) * np.matmul(X.T, np.matmul(X, self.theta) - y)
-        return grad
     
-    def predict(self, testing_set):
-        '''Predicts the response value given the predictors (of testing set) and learned weights'''
-        n = testing_set.shape[0]
-        m = testing_set.shape[1]-1
-        X = np.c_[(np.ones(n), testing_set[0:n, 0:m])]
-        return np.matmul(X, self.theta)
-    
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        '''Predicts the estimated response for the given input'''
+        y_est = np.dot(X, self.theta)
+        return y_est
+        
+        
 if __name__ == "__main__":
-    # Load dataset
-    training_data = pd.read_csv("dataset/l1_train.csv").to_numpy()
-    testing_data = pd.read_csv("dataset/l1_test.csv").to_numpy()
+    # Handle csv file
+    training_set = pd.read_csv('./dataset/l1_train.csv').to_numpy()
+    testing_set = pd.read_csv('./dataset/l1_test.csv').to_numpy()
+    m = training_set.shape[1]-1 # Number of predictors (features)
     
-    model = LinReg(epoch=10)
-    model.fit(training_data)
+    # Model training and results
+    model = LinReg(epochs=4, learning_rate=0.001)
+    model.fit(X_features=training_set[:,:m], y=training_set[:,m:])
+    print("Model weights: ", model.theta)
+    print("Cost history: ", model.cost_history)
     
-    # Handling training dataset
-    n = training_data.shape[0]
-    m = training_data.shape[1]-1
-    x = training_data[0:n, 0:m]
-    y = training_data[:, -1:]
-
-    # Handling testing dataset
-    n_test = testing_data.shape[0]
-    m_test = testing_data.shape[1]-1
-    X_test = np.c_[(np.ones(n_test), testing_data[0:n_test, 0:m_test])] # Design matrix for features of testing set
-    y_test= testing_data[:, -1:]
-  
-    test = model.cost_function(n_test, X_test, y_test)
-    print()
-    print()
-    print(test)
     
-    # Implement Scikit, compare results
-    scikit_model = linear_model.LinearRegression()
-    scikit_model.fit(x, y)
-    print("Scikit learn model weights:", scikit_model.coef_, "Bias:", scikit_model.intercept_)
-    print("Custom model weights:", model.theta)
+    # Compare with sklearn implementation
+    reg = linear_model.LinearRegression().fit(training_set[:,:m], training_set[:,m:])
+    print("Sklearn weights: ", reg.coef_, reg.intercept_)
     
+    # Visualize the estimated model
+    plt.scatter(training_set[:,:m], training_set[:,m:], color="blue", label="Training set")
+    plt.plot(training_set[:,:m], model.predict(np.c_[np.ones(training_set.shape[0]), training_set[:,:m]]), color="red", label="Model")
+    
+    plt.xlabel("X")
+    plt.ylabel("Y")
+    plt.title("Linear Regression")
+    plt.legend()
+    plt.show()
