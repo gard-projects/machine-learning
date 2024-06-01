c8389e557c6ebcca3865231717c4ffd6b6623e4b
gardgundersen44 <gardgundersen4@gmail.com>
2024-06-01 13:02:09 +0200

    v 2.0.6

--- ShallowLearning/KNN/KNN.py
+++ ShallowLearning/KNN/KNN.py
@@ -8,48 +8,120 @@ from sklearn.compose import ColumnTransformer
 from category_encoders import TargetEncoder
 import pandas as pd
 
-# K-Nearest Neighbors (KNN) algorithm implementation
+
 class KNN:
+    '''
+    A class used to present a K-Nearest Neighbors model
     
+    Attributes
+    ----------
+    k : int
+        Number of neighbors to consider
+    model_accuracy : float
+        Accuracy of the model on the testing set
+    X_features : np.ndarray
+        A matrix with n samples and m predictors, shape (n, m) for training set
+    y : np.ndarray
+        A vector with n responses, shape (n, 1) for training set
+    testing_set : np.ndarray
+        A matrix with n samples and m predictors, shape (n, m) for testing set
+    mean : np.ndarray
+        Mean of the numerical features
+    std : np.ndarray
+        Standard deviation of the numerical features
+    
+    Methods
+    -------
+    fit(training_set, testing_set, distance_metric='euclidean')
+        Fits the KNN model, performs scaling, encoding, and computes the accuracy
+    encode_features(features, m, encoded_y, set_type='training')
+        Encodes the categorical features for both training and testing sets
+    euclidean_distance(x1, x2, batch_size=1000)
+        Computes the Euclidean distance between two matrices, using batch processing
+    manhattan_distance(x1, x2, batch_size=1000)
+        Computes the Manhattan distance between two matrices, using batch processing
+    one_hot_encoding(column_index, features=None, set_type='training')
+        Encodes the categorical features using one-hot encoding
+    label_encoding(column)
+        Encodes the response variable using label encoding
+    target_encoding(column_index, target, features=None, set_type='training')
+        Encodes the categorical features using target encoding
+    predict(testing_set, distance_metric)
+        Predicts the labels for the samples in the testing set
+    accuracy(actual, predicted)
+        Computes the accuracy of the model 
+    '''
     
     def __init__(self, k: int):
+        """
+        Parameters
+        ----------
+        k : int
+            Number of neighbors to consider
+        """
         self.k = k
         self.model_accuracy = 0
         
     
-    
-    def fit(self, training_set: np.ndarray, testing_set: np.ndarray, 
-            distance_metric: str='euclidean'):
-        m = training_set.shape[1]-1 # Number of predictors
+    def fit(self, training_set: np.ndarray, testing_set: np.ndarray, distance_metric: str='euclidean'):
+        '''
+        The main method to fit the KNN model, perform scaling, encoding, and compute the accuracy
+        
+        Parameters
+        ----------
+        training_set: np.ndarray
+            A matrix with n samples and m predictors, shape (n, m) for training set
+        testing_set: np.ndarray
+            A matrix with n samples and m predictors, shape (n, m) for testing set
+        distance_metric: str
+            The distance metric to use, either 'euclidean' or 'manhattan'
+        '''
+        
+        m = training_set.shape[1] - 1  # Number of predictors
         
         self.X_features = training_set[:, :m]
         self.y = training_set[:, m]
         self.testing_set = testing_set
-        self.actual_response = testing_set[:, m:]
+        self.actual_response = testing_set[:, m]
+        
         encoded_y = self.label_encoding(self.y)
         
-        # Encode categorical features
-        self.encode_features(None, m, encoded_y)
+        numerical_features = [0, 2, 4, 10, 11, 12]
+        self.mean = np.mean(self.X_features[:, numerical_features].astype(float), axis=0)
+        self.std = np.std(self.X_features[:, numerical_features].astype(float), axis=0)
+        
+        self.X_features[:, numerical_features] = (self.X_features[:, numerical_features] - self.mean) / self.std
+        self.testing_set[:, numerical_features] = (self.testing_set[:, numerical_features] - self.mean) / self.std
+        
+        self.encode_features(self.X_features, m, encoded_y)
         self.encode_features(self.testing_set[:, :m], m, encoded_y, 'testing')
-
-        # Implement KNN algorithm (compute distances and find k nearest neighbors)
-        self.predict(self.testing_set, distance_metric)   
+        
+        self.predict(self.testing_set, distance_metric) 
     
     
     def encode_features(self, features: np.ndarray , m: int, encoded_y: np.ndarray, set_type: str='training'):
-       # Encoding variables
+        '''
+        Encodes the categorical features for both training and testing sets
+        
+        Parameters
+        ----------
+        features: np.ndarray
+            A matrix with n samples and m predictors, shape (n, m), represents the categorical variables of the testing set
+        m: int
+            Number of predictors
+        encoded_y: np.ndarray
+            Encoded response variable
+        set_type: str
+            The type of the set, either 'training' or 'testing'   
+        '''
         indices_one_hot = [1, 5, 7, 8, 9]
         indices_target_encoding = [3, 6, 13]
-        # Standardize numerical features
-        numerical_features = [0, 2, 4, 10, 11, 12]
 
+        # Training set
         if set_type == 'training':
             encoded_columns = np.zeros((self.X_features.shape[0],0))
             remaining_columns = np.zeros((self.X_features.shape[0],0))
-           
-           # Standardize numerical features ....
-            
-        
+                        
             for i in range(m):
                 if i in indices_one_hot:
                     encoded_columns = np.concatenate((encoded_columns, self.one_hot_encoding(i)), axis=1)
@@ -60,9 +132,10 @@ class KNN:
             self.X_features = np.concatenate((encoded_columns, remaining_columns), axis=1)
         
         else:
+            # Testing set
             encoded_columns = np.zeros((features.shape[0],0))
             remaining_columns = np.zeros((features.shape[0],0))
-        
+                
             for i in range(m):
                 if i in indices_one_hot:
                     encoded_columns = np.concatenate((encoded_columns, self.one_hot_encoding(i, features, 'testing')), axis=1)
@@ -72,8 +145,25 @@ class KNN:
                     remaining_columns = np.concatenate((remaining_columns, features[:, i].reshape(-1,1)), axis=1)
             self.testing_set = np.concatenate((encoded_columns, remaining_columns), axis=1)
 
-    def euclidean_distance(self, x1: np.ndarray, x2: np.ndarray, batch_size: int=1000):
-        # Use batch processing to compute Euclidean distance, due to the large number of samples
+    def euclidean_distance(self, x1: np.ndarray, x2: np.ndarray, batch_size: int=1000) -> np.ndarray:
+        '''
+        Computes the Euclidean distance between two matrices, using batch processing
+        
+        Parameters
+        ----------
+        x1: np.ndarray
+            A matrix with n samples and m predictors, shape (n, m), represents the testing set
+        x2: np.ndarray
+            A matrix with n samples and m predictors, shape (n, m), represents the training set
+        batch_size: int
+            The size of the batch to use for processing
+            
+        Returns
+        -------
+        distances: np.ndarray
+            A matrix with n samples and m predictors, shape (n, m), represents the Euclidean distances
+        '''
+        
         m, n = x1.shape[0], x2.shape[0]
         distances = np.empty((m, n))
       
@@ -85,19 +175,69 @@ class KNN:
                 end_j_index = min(j + batch_size, n)
                 x2_batch = x2[j:end_j_index, :]
                 
-                # Compute distances, result is a 2D numpy array
                 dist_batch = np.sqrt(np.sum((x1_batch[np.newaxis, :, :].astype(float) - x2_batch[:, np.newaxis, :].astype(float))**2,axis=2))
                 distances[i:end_i_index, j:end_j_index] = dist_batch.T
+        
         return distances
     
-    def manhattan_distance(self, x1: np.ndarray, x2: np.ndarray):
-        d = np.sum(np.abs(x1-x2), axis=1)
-        return d
+    def manhattan_distance(self, x1: np.ndarray, x2: np.ndarray, batch_size: int=1000) -> np.ndarray:
+        '''
+        Computes the Manhattan distance between two matrices, using batch processing
+        
+        Parameters
+        ----------
+        x1: np.ndarray
+            A matrix with n samples and m predictors, shape (n, m), represents the testing set
+        x2: np.ndarray
+            A matrix with n samples and m predictors, shape (n, m), represents the training set
+        batch_size: int
+            The size of the batch to use for processing
+            
+        Returns
+        -------
+        distances: np.ndarray
+            A matrix with n samples and m predictors, shape (n, m), represents the Manhattan distances
+        
+        '''
+        m, n = x1.shape[0], x2.shape[0]
+        distances = np.empty((m, n))
+
+        for i in range(0, m, batch_size):
+            end_i_index = min(i + batch_size, m)
+            x1_batch = x1[i:end_i_index, :]
+
+            for j in range(0, n, batch_size):
+                end_j_index = min(j + batch_size, n)
+                x2_batch = x2[j:end_j_index, :]
+
+                # Compute Manhattan distances
+                dist_batch = np.sum(np.abs(x1_batch[:, np.newaxis, :].astype(float) - x2_batch[np.newaxis, :, :].astype(float)), axis=2)
+                distances[i:end_i_index, j:end_j_index] = dist_batch
+
+        return distances
+
 
         c = np.dot(x1, x2.T) / (np.linalg.norm(x1, axis=1) * np.linalg.norm(x2))
         return c
     
     def one_hot_encoding(self, column_index: int, features: np.ndarray=None, set_type: str='training'):
+        '''
+        Encodes the categorical features using one-hot encoding
+        
+        Parameters
+        ----------
+        column_index: int
+            The index of the column to encode
+        features: np.ndarray
+            A matrix with n samples and m predictors, shape (n, m), represents the testing set (default is None)
+        set_type: str
+            The type of the set, either 'training' or 'testing' (default is 'training')
+            
+        Returns
+        -------
+        encoded_column: np.ndarray
+            A matrix with n samples and m predictors, shape (n, m), represents the encoded column
+        '''
         if set_type == 'training':
             column = self.X_features[:, column_index]
         else: 
@@ -111,12 +251,44 @@ class KNN:
         return encoded_column
            
     def label_encoding(self, column: np.ndarray) -> np.ndarray:
+        '''
+        Encodes the response variable using label encoding
+        
+        Parameters
+        ----------
+        column: np.ndarray
+            A vector with n responses, shape (n, 1)
+        
+        Returns
+        -------
+        result: np.ndarray
+            A vector with n responses, shape (n, 1), represents the encoded response variable
+        '''
         u = np.unique(column)
         v = {value: i for i, value in enumerate(u)}
         result = np.array([v[value] for value in column])
         return result
     
     def target_encoding(self, column_index: int, target: np.ndarray, features: np.ndarray=None, set_type: str='training'):
+        '''
+        Encodes the categorical features using target encoding
+        
+        Parameters
+        ----------
+        column_index: int
+            The index of the column to encode
+        target: np.ndarray
+            A vector with n responses, shape (n, 1)
+        features: np.ndarray
+            A matrix with n samples and m predictors, shape (n, m), represents the testing set (default is None)
+        set_type: str
+            The type of the set, either 'training' or 'testing' (default is 'training')
+        
+        Returns
+        -------
+        encoded_column: np.ndarray
+            A matrix with n samples and m predictors, shape (n, m), represents the encoded column
+        '''
         if set_type == 'training':
             column = self.X_features[:, column_index]
         else:
@@ -128,63 +300,18 @@ class KNN:
                 encoded_column[np.where(column == u)] = np.mean(target[np.where(column == u)])
         
         return encoded_column
-    
-    def k_folds(self, data: np.ndarray):
-        # Shuffle samples in dataset
-        np.random.shuffle(data)
-        
-        # Split data into k folds
-        fold_size = data.shape[0] // self.k
-        # An array of k folds
-        folds = [data[i*fold_size:(i+1)*fold_size] for i in range(self.k)]
-        
-        # Check if data can be split in equal folds
-        if data.shape[0] % self.k != 0:
-            folds[-1] = np.concatenate((folds[-1], data[self.k*fold_size:]))
-        return folds
-    
-    def k_fold_target_encoding(self, column: np.ndarray, target: np.ndarray):
-        column_folds = self.k_folds(column, self.k)
-        target_folds = self.k_folds(target, self.k)
-        result = np.zeros(column.shape)
-        
-        for i in range(self.k):
-            # Step 1: Split data into training and validation sets
-            training_data = np.concatenate(column_folds[j] for j in range(self.k) if j != i)
-            training_target = np.concatenate(target_folds[j] for j in range(self.k) if j != i)
-            validation_data = column_folds[i]
-            
-            # Step 2: Find the unique categories in the training data
-            unique_values = np.unique(training_data)
-            
-            # Step 3: Calculate the mean of the target variable for each category
-            for u in unique_values:
-                mean_target = np.mean(training_target[training_data == u]) # Boolean mask
-                result[validation_data == u] = mean_target
-        return result
      
-    def leave_one_out_target_encoding(self, column: np.ndarray, target: np.ndarray):
-        unique_categories = np.unique(column)
-        
-        # Map each category to respective indices in target variable
-        category_indices = {c: np.where(column==c) for c in unique_categories}
-        column_encoded = np.zeros_like(target)
-        
-        for category, indices in category_indices.items():
-            category_sum = np.sum(target[indices])
-            category_count = len(indices)-1
-            
-            if category_count == 0:
-                # If there is only one sample in the category, use the global mean
-                column_encoded[indices] = np.mean(target)
-            else:
-                # More than one sample in the category
-                for i in indices:
-                    column_encoded[i] = (category_sum - target[i]) / category_count
-            
-        return column_encoded
-    
     def predict(self, testing_set: np.ndarray, distance_metric: str):
+        '''
+        Finds the K nearest neighbors and predicts the labels for the samples in the testing set
+        
+        Parameters
+        ----------
+        testing_set: np.ndarray
+            A matrix with n samples and m predictors, shape (n, m) for testing set
+        distance_metric: str
+            The distance metric to use, either 'euclidean' or 'manhattan'
+        '''
         m = testing_set.shape[1]-1
         actual_response = testing_set[:, m:]
         distances = None
@@ -192,6 +319,8 @@ class KNN:
         
         if distance_metric == 'euclidean':
             distances = self.euclidean_distance(testing_set, self.X_features)
+        elif distance_metric == 'manhattan':
+            distances = self.manhattan_distance(testing_set, self.X_features)
             
         # Find the k nearest neighbors
         neighbors = np.argsort(distances, axis=1)[:, :self.k]
@@ -204,6 +333,21 @@ class KNN:
         self.model_accuracy = self.accuracy(self.actual_response, np.array(predicted_response))
     
     def accuracy(self, actual: np.ndarray, predicted: np.ndarray):
+        '''
+        Computes the accuracy of the model
+        
+        Parameters
+        ----------
+        actual: np.ndarray
+            A vector with n responses, shape (n, 1)
+        predicted: np.ndarray
+            A vector with n responses, shape (n, 1)
+        
+        Returns
+        -------
+        mean: float
+            The estimated accuracy of the model
+        '''
         return np.mean(actual == predicted)
     
      
@@ -232,7 +376,7 @@ if __name__ == '__main__':
         ]
     )
     
-    # Model pipeline
+    # Pipeline for data preprocessing and model training
     pipeline = Pipeline([
         ('preprocessor', preprocessor),
         ('classifier', KNeighborsClassifier(n_neighbors=5))
